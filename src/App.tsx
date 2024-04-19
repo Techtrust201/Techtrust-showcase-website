de060cc017bbf0dfdfb9c8350effd04aede72e1e
HugoPortier <hugo_portier@hotmail.com>
2024-04-19 08:56:39 +0200

    formate prettier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div data-testid="app-root">
-      <h1>Welcome to Techtrust Showcase Website</h1>
+      <h1>Welcome to Techtrust Showcase Websitffe</h1>
       {/* Le reste de votre application ira ici. */}
     </div>
   );
